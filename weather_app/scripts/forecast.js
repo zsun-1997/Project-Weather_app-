a61fa17821bc93a6861ca102a0942c5aac2198bd
zsun-1997 <zsun@laurentian.ca>
2022-06-10 12:52:57 -0400

    second vision

--- weather_app/scripts/forecast.js
+++ weather_app/scripts/forecast.js
@@ -1,11 +1,23 @@
 const key = "Gx6EdCo9FAtCDDR4U1AfeBCPBHA3hGHr";
 
-const getCity = async (city) => {
-  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
-  const query = `?api=${key}&q=${city}`;
+// get weather information
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
   const response = await fetch(base + query);
   const data = await response.json();
-  console.log(data);
+
+  return data[0];
 };
 
-getCity("toronto");
+// get city information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
